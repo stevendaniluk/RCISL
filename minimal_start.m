84c3a0599c043479155bd9ec3c237880dab5f944
Steven Daniluk <stevendaniluk8@gmail.com>
2016-05-05 18:18:54 -0400

    Update from changes to ExecutiveSimulation

--- minimal_start.m
+++ minimal_start.m
@@ -23,8 +23,8 @@ disp(['Number of iterations: ',num2str(Simulation.world_state_.iterations)])
 toc
 
 %% Make consecutive runs
-% num_runs = 2;
-% save_data = true;
-% sim_name = 'test_1';
-% 
-% Simulation.consecutiveRuns(config, num_runs, save_data, sim_name);
+num_runs = 50;
+save_data = true;
+sim_name = 'test';
+
+Simulation.consecutiveRuns(num_runs, save_data, sim_name);
