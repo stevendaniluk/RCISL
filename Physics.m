212d966a3cefdf38fb20fb6ff837b8294e87fa68
Steven Daniluk <stevendaniluk8@gmail.com>
2016-05-27 18:16:42 -0400

    Rewrite validPoint
    -Eliminate collide method (not necessary)
    -Add comments

--- Physics.m
+++ Physics.m
@@ -1,5 +1,15 @@
 classdef Physics
     %PHYSICS - Responsible for all physics in RCISL simulation
+    
+    % Peforms modifications to the worldstate when robots move forward,
+    % backward, rotate, or interact with an item. 
+    %
+    % When the robot attempts to move forward or backward it will check if 
+    % the desired movement is valid by checking for collisions with objects 
+    % in the world.
+    %
+    % Interction involves picking up an item, or dropping an item if the
+    % robot is carrying an item.
         
     properties
         config_ = [];
@@ -41,13 +51,13 @@ classdef Physics
                     world_state.targetProperties(target_id, world_state.tpid_carriedBy) = 0;
                 else
                     % Check proximity
-                    posDiff = world_state.robot_pos_(robot_id,:) - world_state.target_pos_(target_id,:);
-                    posDiff = sqrt(posDiff.^2);
-                    posDiff = sum(posDiff);
-                    robotReach = world_state.robotProperties(robot_id,6);
+                    dist = world_state.robot_pos_(robot_id,:) - world_state.target_pos_(target_id,:);
+                    dist = sqrt(dist.^2);
+                    dist = sum(dist);
+                    robot_reach = world_state.robotProperties(robot_id,6);
                     
                     % Pick up the item of close enough
-                    if(posDiff <= robotReach)
+                    if(dist <= robot_reach)
                         world_state.targetProperties(target_id,world_state.tpid_carriedBy) = robot_id;
                     end
                 end
@@ -80,7 +90,7 @@ classdef Physics
                 new_pos = world_state.robot_pos_(robot_id,:) + new_vel;
                 
                 % Only move if its valid
-                if this.validPoint(world_state, new_pos, world_state.TYPE_ROBOT, robot_id, 1, 0) == 1
+                if this.validPoint(world_state, new_pos, robot_id) == 1
                     world_state.robot_pos_(robot_id,:) = new_pos;
                     world_state.robot_vel_(robot_id,:) = new_vel;
                     
@@ -129,224 +139,90 @@ classdef Physics
         %   validPoint
         %   
         %   Test if a new point is valid, in terms of collision,
-        %   if the object with the current id and type is taken
-        %   and moved toward the newPoint.
+        %   if the robot with the current id is taken and moved to the
+        %   new point
+        %
+        %   INPUTS:
+        %   world_state = WorldState object
+        %   new_point = Vector [X, Y, Z] with new point coordinates
+        %   robot_id = ID number of robot
   
-        function valid = validPoint(this, world_state, newPoint,type,id,doCollide,partnerId)
-            %find the size to be used
-            myRobot1 = 0;
-            myRobot2 = 0;
-            
-            % Assign sizes if we are a robot, obstacle, or target
-            if type == 1
-                % Obstacle
-                mySize = world_state.obstacle_size_;
-                myVelocity = world_state.obstacle_vel_(id,:);
-                myPos = world_state.obstacle_pos_(id,:);
-                myMass = world_state.obstacle_mass_;
-                myStrength = 1;
-            elseif type == 2
-                % Robot
-                mySize = world_state.robot_size_;
-                myVelocity = world_state.robot_vel_(id,:);
-                myPos = world_state.robot_pos_(id,:);
-                myMass = world_state.robot_mass_;
-                myStrength = world_state.robotProperties(id,3);
-            elseif type == 3
-                % Target
-                mySize = world_state.target_size_;
-                myVelocity = world_state.target_vel_(id,:);
-                myPos = world_state.target_pos_(id,:);
-                myMass = world_state.target_mass_;
-                myStrength = 1;
-
-                if(world_state.groupPickup == 1)
-                    myRobot1 = world_state.targetProperties(id,world_state.ID_CARRIED_BY);
-                    myRobot2 = world_state.targetProperties(id,world_state.ID_CARRIED_BY_2);
-                end
-            end
+        function valid = validPoint(~, world_state, new_point, robot_id)
+            % Get size of robot
+            robot_size = world_state.robot_size_;
             
             %Test X world boundary
-            if ((newPoint(1) - mySize < 0) || (newPoint(1) + mySize > world_state.world_width_))
-                valid=0; 
+            if ((new_point(1) - robot_size < 0) || (new_point(1) + robot_size > world_state.world_width_))
+                valid = false; 
                 return; 
             end
+            
             %Test Y world boundary
-            if ((newPoint(2) - mySize < 0) || (newPoint(2) + mySize > world_state.world_height_))
-                valid=0; 
+            if ((new_point(2) - robot_size < 0) || (new_point(2) + robot_size > world_state.world_height_))
+                valid = false; 
                 return; 
             end
+            
             % Test Z world boundary
-            if ((newPoint(3) < 0) || (newPoint(3)  > world_state.world_depth_))
-                valid=0; 
+            if ((new_point(3) < 0) || (new_point(3)  > world_state.world_depth_))
+                valid = false; 
                 return; 
             end
             
-            %Test against Obstacles
-            obsDist = bsxfun(@minus,world_state.obstacle_pos_, newPoint);
+            % Get distances to obstacles
+            obs_dist = bsxfun(@minus,world_state.obstacle_pos_, new_point);
+            obs_dist = sqrt(obs_dist(:,1).^2 + obs_dist(:,2).^2 + obs_dist(:,3).^2);
+            [min_obs_dist, ~] = min(obs_dist);
             
-            if  type == 1 ; obsDist(id,:) = [100 100 100]; end; 
-            obsDist = obsDist(:,1).^2 + obsDist(:,2).^2 + obsDist(:,3).^2;
-            obsDist = sqrt(obsDist);
-            [minDist,closestObstacleId] = min(obsDist);
-
-            %Test against Robots (other robots)
-            robDist = bsxfun(@minus,world_state.robot_pos_, newPoint);
-            if(partnerId > 0)
-                robDist(partnerId,:)  = robDist(partnerId,:) + 400;
-            end
-            
-            if(world_state.groupPickup == 1)
-                %dont consider bumping into attached robots
-                if(myRobot1 > 0)
-                    robDist(myRobot1,:)  = robDist(myRobot1,:) + 200;
-                end
-                if(myRobot2 > 0)
-                    robDist(myRobot2,:)  = robDist(myRobot2,:) + 300;
-                end
-            end
-            
-            if type==2 ; robDist(id,:) = [100 100 100]; end;
-            robDist = robDist(:,1).^2 + robDist(:,2).^2 + robDist(:,3).^2;
-            robDist = sqrt(robDist);
-            [minDistRobot,closestRobotId] = min(robDist);
-
-            %Test against Targets
-            targetDist = bsxfun(@minus,world_state.target_pos_, newPoint);
-            if  type == 3 ; targetDist(id,:) = [100 100 100]; end; 
-
-            %next lines "moves" targets that are returned.
-            targetDist = bsxfun(@plus,targetDist, (abs(world_state.targetProperties(:,1)).*100));
-
-            %next lines "moves" targets that are being carried.
-            if(world_state.boxPickup == 1 || world_state.groupPickup == 1)
-                targetDist = bsxfun(@plus,targetDist, (abs(world_state.targetProperties(:,world_state.ID_CARRIED_BY)).*100));
-
-            end
-            if(world_state.groupPickup == 1) 
-                targetDist = bsxfun(@plus,targetDist, (abs(world_state.targetProperties(:,world_state.ID_CARRIED_BY_2)).*100));                
-            end            
-            
-            targetDist = targetDist(:,1).^2 + targetDist(:,2).^2 + targetDist(:,3).^2;
-            targetDist = sqrt(targetDist);
-            [minTargetDist,closestTargetId] = min(targetDist) ;            
-            
-            if minDist < mySize + world_state.obstacle_size_
-                valid = 0;
-                %preform a collision and update the velocities
-                physicsArray1 = [world_state.obstacle_pos_(closestObstacleId,:) world_state.obstacle_vel_(closestObstacleId,:) world_state.obstacle_size_ world_state.obstacle_mass_];
-                physicsArray2 = [myPos myVelocity mySize myMass];
-                [physicsArray1,physicsArray2] = this.Collide(physicsArray1,physicsArray2);   
-                if (doCollide == 1)
-                    world_state.obstacle_pos_(closestObstacleId,:) = physicsArray1(1:3);
-                    world_state.obstacle_vel_(closestObstacleId,:) = physicsArray1(4:6);
-                end
-                myPos = physicsArray2(1:3);
-                myVelocity = physicsArray2(4:6);
+            % Check for collision with obstacles
+            if min_obs_dist < robot_size + world_state.obstacle_size_
+                valid = false;
                 return;
             end
             
-            if minDistRobot < mySize + world_state.robot_size_
-                valid = 0;
-                %preform a collision and update the velocities
-                physicsArray1 = [world_state.robot_pos_(closestRobotId,:) world_state.robot_vel_(closestRobotId,:) world_state.robot_size_ world_state.robot_mass_];
-                physicsArray2 = [myPos myVelocity mySize myMass];
-                [physicsArray1,physicsArray2] = this.Collide(physicsArray1,physicsArray2);
-                if(doCollide == 1)
-                    world_state.robot_pos_(closestRobotId,:) = physicsArray1(1:3);
-                    world_state.robot_vel_(closestRobotId,:) = physicsArray1(4:6);
-                end
-                myPos = physicsArray2(1:3);
-                myVelocity = physicsArray2(4:6);
+            % Get distances to other robots (must remove this robot from
+            % array of distances)
+            robot_dist = bsxfun(@minus,world_state.robot_pos_, new_point);
+            robot_dist(robot_id, :) = [];
             
-                return;
-            end
-            
-            if minTargetDist < mySize + world_state.target_size_
-                valid = 0;                
-                %preform a collision and update the velocities
-                boxMass = world_state.targetProperties(closestTargetId,2);
+            if(~isempty(robot_dist))
+                robot_dist = sqrt(robot_dist(:,1).^2 + robot_dist(:,2).^2 + robot_dist(:,3).^2);
+                [min_robot_dist, ~] = min(robot_dist);
                 
-                %Alter weights so a weak robot can't budge a heavy box
-                robotType = world_state.robotProperties(id,5);
-                boxType = world_state.targetProperties(closestTargetId,3);
-                if(boxType == 2) 
-                    %if the box is heavy
-                    if(robotType == 2 || robotType == 3) 
-                        %And you are weak, and can't budge it
-                        valid = 0;
-                        return;    
-                    end
+                % Check for collision with other robots
+                if min_robot_dist < robot_size + world_state.robot_size_
+                    valid = false;
+                    return;
                 end
-
-                physicsArray1 = [world_state.target_pos_(closestTargetId,:) world_state.target_vel_(closestTargetId,:) world_state.target_size_ boxMass];
-                physicsArray2 = [myPos myVelocity mySize myMass*myStrength];
-                [physicsArray1,physicsArray2] = this.Collide(physicsArray1,physicsArray2);
-                if(doCollide == 1)
-                    world_state.target_pos_(closestTargetId,:) = physicsArray1(1:3);
-                    world_state.target_vel_(closestTargetId,:) = physicsArray1(4:6);
-                end
-                myPos = physicsArray2(1:3);
-                myVelocity = physicsArray2(4:6);
+            end
+            
+            % Get distance to other targets
+            target_dist = bsxfun(@minus,world_state.target_pos_, new_point);
+            
+            % Don't check against items being carried or returned
+            returned_items = (world_state.targetProperties(:,1) ~= 0);
+            carried_items = (world_state.targetProperties(:,world_state.ID_CARRIED_BY) ~= 0);
+            target_dist(returned_items, :) = [];
+            target_dist(carried_items, :) = [];
+            
+            if(~isempty(target_dist))
+                target_dist = sqrt(target_dist(:,1).^2 + target_dist(:,2).^2 + target_dist(:,3).^2);
+                [min_target_dist, closest_target] = min(target_dist) ;
                 
-                return;
-            end
-            valid = 1;
-            
-        end
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % 
-        %   Collide
-        %   
-        %   Collide a set of two similar objects
-  
-        function [phyResult1,phyResult2]= Collide(~,physicsArray1,physicsArray2)
-            %first lets get complete vectors
-            pa1 = physicsArray1;
-            pa2 = physicsArray2;
-            %first lets find the vector between them, from 1->2
-            b = pa2(1:3) - pa1(1:3);
-            
-            if(sum(b,2) == 0)
-                b = [1 0 0];
+                % Check if the robot is at the same position as an item
+                % (for cases when it drops an item)
+                coincident = sum(world_state.robot_pos_(robot_id,:) - world_state.target_pos_(closest_target,:)) == 0;
+                
+                % Check for collision with targets (must allow for the case
+                % when a robot drops an item, making the distance zero)
+                if ((min_target_dist < (robot_size + world_state.target_size_)) && ~coincident)
+                    valid = false;
+                    return;
+                end
             end
             
-            %velocities and mass
-            v1 = pa1(4:6);
-            v2 = pa2(4:6);
-            m1 = pa1(8);
-            m2 = pa2(8);
-            if(m1 == 0)
-                m1 = 10000;
-            end
-            if(m2 == 0)
-                m2 = 10000;
-            end
-
-            b = b(:)./sqrt(b(1)^2 + b(2) ^2 + b(3) ^2);
-
-            v1a = ((v1*b)*b)';
-            v1b = v1 - v1a;
-
-            v2a = ((v2*b)*b)';
-            v2b = v2 - v2a;
-
-            vf1a = ((m1 -  m2).*v1a + 2*(m2.*v2a))./(m1+m2);
-
-            vf2a = ((m2 -  m1).*v2a + 2*(m1.*v1a))./(m1+m2);
-            vf1 = vf1a+ v1b;
-            vf2 = vf2a+ v2b;
-            if(m1 == 0)
-                vf1 = 0;
-            end
-            if(m2 == 0)
-                vf2 = 0;
-            end
-            
-            phyResult1 = [physicsArray1(1:3) vf1 physicsArray1(7:8)];
-            phyResult2 = [physicsArray2(1:3) vf2 physicsArray2(7:8)];
-
+            % If all chacks have passed, the new point is valid
+            valid = true;
         end
 
     end
