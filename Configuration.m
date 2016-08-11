75ada6cb5d388568ac2d59c797136f78ffce1290
Steven Daniluk <stevendaniluk8@gmail.com>
2016-08-11 17:59:37 -0400

    New parameter set

--- Configuration.m
+++ Configuration.m
@@ -3,14 +3,14 @@ classdef Configuration < handle
     properties
         
         % Graphics Parameters
-        show_live_graphics = true;
+        show_live_graphics = false;
         show_track_graphics = false;
                         
         % Primary Scenario Parameters
         max_iterations = 5000;
-        numRobots    = 3;
+        numRobots    = 4;
         numObstacles = 4;
-        numTargets   = 3;
+        numTargets   = 4;
         robot_Type =[ 4*pi/18      2         0.30      1; ... 
                       4*pi/18      1         0.40      2; ... 
                       4*pi/18      1         0.30      3; ... 
@@ -22,8 +22,8 @@ classdef Configuration < handle
         % Action and State Parameters
         num_actions = 5;
         num_state_vrbls = 5;
-        num_state_bits = [4, 1, 4, 4, 4];
-        backup_fractional_speed = 0.0;
+        num_state_bits = [4, 2, 4, 4, 4];
+        backup_fractional_speed = 1.0;
         look_ahead_dist = 1.5;
         
         % World Parameters
@@ -40,7 +40,6 @@ classdef Configuration < handle
         target_size = 0.25;
         target_mass = 1;
         goal_size = 1;
-        simulation_Realism = 0;
         
         % Noise and Particle Filter Parameters
         noise_sigma = 0.0;
@@ -62,27 +61,27 @@ classdef Configuration < handle
         reward_activation_dist = 0.17;
         
         % Policy parameters
-        policy = 'e-greedy'; % Options: "greedy", "e-greedy", "softmax"
+        policy = 'softmax'; % Options: "greedy", "e-greedy", "softmax"
         e_greedy_epsilon = 0.10;
         softmax_temp = 0.05;        % Temperature for softmax distribution
         
         % Q-Learning Parameters
         gamma = 0.3;            % Discount factor
         alpha_max = 0.9;        % Maximum value of learning rate
-        alpha_denom = 30;       % Coefficient in alpha update equation
-        alpha_power = 1;        % Coefficient in alpha update equation
+        alpha_denom = 300;      % Coefficient in alpha update equation
+        alpha_power = 2;        % Coefficient in alpha update equation
         
         % Team Learning Parameters
-        task_allocation = 'fixed';  % Options: "fixed", "l_alliance"
+        task_allocation = 'l_alliance';  % Options: "fixed", "l_alliance"
         
         % L-Alliance Parameters
         motiv_freq = 5;             % Frequency at which motivation updates
-        max_task_time = 2000;       % Maximum time on task before acquescing
+        max_task_time = 5000;       % Maximum time on task before acquescing
         trial_time_update = 'recursive_stochastic'; % Options: "moving_average", "recursive_stochastic"
-        stochastic_update_theta1 = 1.0; % Coefficient for stochastic update
-        stochastic_update_theta2 = 0.9; % Coefficient for stochastic update
-        stochastic_update_theta3 = 1.0; % Coefficient for stochastic update
-        stochastic_update_theta4 = 2.5; % Coefficient for stochastic update
+        stochastic_update_theta1 = 1.0;  % Coefficient for stochastic update
+        stochastic_update_theta2 = 15.0; % Coefficient for stochastic update
+        stochastic_update_theta3 = 0.3;  % Coefficient for stochastic update
+        stochastic_update_theta4 = 2.0;  % Coefficient for stochastic update
         
         % Advice Parameters
         advice_on = false;              % If advice should be used
